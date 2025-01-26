05f1a116ea1b24f7f58bae9a005fc9f4208509f2
Ethanscharlie <ethanscharlie@protonmail.com>
2025-01-26 15:49:55 -0700

    Ethans first commit with Defining constants

---
+++ src/constants.py
@@ -0,0 +1,21 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    April Tag Subsystem
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    April Tag Command I guess
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
+SOME_CONSTANT = 2 # Explain what this is for
